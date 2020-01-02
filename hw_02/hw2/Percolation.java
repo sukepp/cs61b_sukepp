21cf23e98badaa653578df3cf5554a2d2d819bf6
sukepp <hanpulous@gmail.com>
2020-01-02 16:04:01 +0000

    Finished hw_02.

--- hw_02/hw2/Percolation.java
+++ hw_02/hw2/Percolation.java
@@ -3,5 +3,113 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private static final int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
+    private boolean[][] grid;
+    private WeightedQuickUnionUF unionFind;
+    /**
+     * Back up unionFind, ONLY connected to the virtual TOP but not BOTTOM.
+     */
+    //private WeightedQuickUnionUF unionFindTopOnly;
+    private int N;
+    private int openCount;
+    private int TOP;
+    private int BOTTOM;
+
+    /**
+     * Create N-by-N grid, with all sites initially blocked
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N: " + N + " <= 0");
+        }
+
+        this.unionFind = new WeightedQuickUnionUF(N * N + 2);
+        //this.unionFindTopOnly = new WeightedQuickUnionUF(N * N + 2);
+        this.grid = new boolean[N][N];
+        this.openCount = 0;
+        this.N = N;
+        this.TOP = 0;
+        this.BOTTOM = 1;
+    }
+
+    private boolean isOutOfBound(int row, int col) {
+        return row < 0 || row >= N || col < 0 || col >= N;
+    }
+
+    private int ufIndex(int row, int col) {
+        return row * N + col + 2;
+    }
+
+    private void unionAround(int row, int col) {
+        int x, y;
+        for (int[] direction : directions) {
+            x = direction[0] + row;
+            y = direction[1] + col;
+            if (!isOutOfBound(x, y) && isOpen(x, y)) {
+                unionFind.union(ufIndex(row, col), ufIndex(x, y));
+                //unionFindTopOnly.union(ufIndex(row, col), ufIndex(x, y));
+            }
+        }
+        if (row == 0) {
+            unionFind.union(TOP, ufIndex(row, col));
+            //unionFindTopOnly.union(TOP, ufIndex(row, col));
+        }
+        if (row == N - 1) unionFind.union(BOTTOM, ufIndex(row, col));
+    }
+
+    /**
+     * Open the site (row, col) if it is not open already.
+     */
+    public void open(int row, int col) {
+        if (isOutOfBound(row, col)) {
+            throw new IllegalArgumentException("(" + row + ", " + col + ") out of bound");
+        }
+        if (grid[row][col]) return;
+
+        grid[row][col] = true;
+        unionAround(row, col);
+        openCount += 1;
+    }
+
+    /**
+     * Is the site (row, col) open?
+     */
+    public boolean isOpen(int row, int col) {
+        if (isOutOfBound(row, col)) {
+            throw new IllegalArgumentException("(" + row + ", " + col + ") out of bound");
+        }
+
+        return grid[row][col];
+    }
+
+    /**
+     * Is the site (row, col) full?
+     * Complexity: O(log(N))
+     */
+    public boolean isFull(int row, int col) {
+        if (isOutOfBound(row, col)) {
+            throw new IllegalArgumentException("(" + row + ", " + col + ") out of bound");
+        }
+
+        if (!isOpen(row, col)) return false;
+        //return unionFind.connected(TOP, ufIndex(row, col))
+        //        && unionFindTopOnly.connected(TOP, ufIndex(row, col));
+        return unionFind.connected(TOP, ufIndex(row, col));
+    }
+
+    /**
+     * Number of open sites
+     */
+    public int numberOfOpenSites() {
+        return openCount;
+    }
+
+    /**
+     * Does the system percolate?
+     * Complexity: O(log(N))
+     */
+    public boolean percolates() {
+        return unionFind.connected(BOTTOM, TOP);
+    }
 }
--- hw_02/hw2/PercolationStats.java
+++ hw_02/hw2/PercolationStats.java
@@ -1,5 +1,117 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
 public class PercolationStats {
 
+    private int N;
+    private int T;
+    private PercolationFactory pf;
+    private double[] results;
+    private double mean;
+    private double stddev;
+    private double totalTimes;
+
+    /**
+     * perform T independent experiments on an N-by-N grid
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N: " + N + " <= 0");
+        }
+        if (T <= 0) {
+            throw new IllegalArgumentException("T: " + T + " <= 0");
+        }
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        this.results = new double[T];
+        this.totalTimes = 0;
+        experiment();
+        this.mean = -1;
+        this.stddev = -1;
+    }
+
+    private void experiment() {
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = pf.make(N);
+            while (!percolation.percolates()) {
+                int x = StdRandom.uniform(N);
+                int y = StdRandom.uniform(N);
+                percolation.open(x, y);
+                totalTimes += 1;
+            }
+            results[i] = 1.0 * percolation.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    /**
+     * Sample mean of percolation threshold.
+     */
+    public double mean() {
+        if (mean < 0) {
+            this.mean = StdStats.mean(results);
+        }
+        return this.mean;
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        if (T == 1) return Double.NaN;
+
+        if (stddev < 0) {
+            this.stddev = StdStats.stddev(results);
+        }
+        return this.stddev;
+    }
+
+    /**
+     * low endpoint of 95% confidence interval
+     */
+    public double confidenceLow() {
+        return mean() / T - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    /**
+     * high endpoint of 95% confidence interval
+     */
+    public double confidenceHigh() {
+        return mean() / T + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double meanTimesPerTrail() {
+        return totalTimes / T;
+    }
+
+    public void printStats() {
+        System.out.println("N: " + N + ", T: " + T);
+        System.out.println("Mean: " + mean());
+        System.out.println("Mean times per trail: " + meanTimesPerTrail());
+    }
+
+    public static void main(String[] args) {
+        int N = 100;
+        int T = 10;
+        Stopwatch timer = new Stopwatch();
+        double preTime = 0;
+        for (int i = 0; i < 20; i++) {
+            Double time1 = timer.elapsedTime();
+            PercolationStats stats = new PercolationStats(N, T, new PercolationFactory());
+            stats.printStats();
+            Double time2 = timer.elapsedTime();
+
+            double curTime = time2 - time1;
+            System.out.println("Total time:" + curTime);
+            System.out.println("Time ratio:" + curTime / preTime);
+            preTime = curTime;
+
+            N *= 2;
+            System.out.println("------------------------------");
+        }
+
+    }
 }
